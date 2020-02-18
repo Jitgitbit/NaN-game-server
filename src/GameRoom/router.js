b1875a11930f5aafa799708e895f6eb655c2c29f
Jitgitbit <rowowolf@gmail.com>
2020-02-18 17:56:13 +0100

    comment

--- src/GameRoom/router.js
+++ src/GameRoom/router.js
@@ -3,4 +3,13 @@ const router = new Router();
 const GameRoom = require(`./model`);
 const db = require(`../db`);
 
+// router.post(`/gamerooms`, (req, res, next) => {
+//   console.log(req.body)
+//   GameRoom.create(req.body)
+//     .then(gameRoom => {
+//       res.send(gameRoom);
+//     })
+//     .catch(err => next(err))
+// })
+
 module.exports = router;
