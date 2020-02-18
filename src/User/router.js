e31bc0d9a97dffa80c527462034bc7d8ed5f93fb
Jitgitbit <rowowolf@gmail.com>
2020-02-18 11:49:37 +0100

    trying join put

--- src/User/router.js
+++ src/User/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const User = require("./model");
 const bcrypt = require("bcrypt");
 const { toJWT } = require("../auth/jwt");
+const auth = require("../auth/middleWare");
 
 const router = new Router();
 
@@ -50,28 +51,35 @@ router.post("/login", async (request, response) => {
   }
 });
 
-// router.put('/user/:id', async (request,response,next) => {
-//   console.log(request.body.email);
+// router.put(
+//   '/user',
+//   (request, response, next) => User
+//     .find(request.params.email)
+//     .then(user => user.update({ gameroomId: request.body.gameroomId}))
+//     .then(user => response.send(user))
+//     .catch(next)
+// )
 
-//   const user = await User.find({where: {email: request.body.email}})
-//     .on(`success`, function(user){
-//       if (user) {
-//         user.update({
-//           gameroomId: request.body.gameroomId
-//         })
-//         .success(function() {})
-//       }
-//     })
+// router.put(
+//   '/join', 
+//   (request, response, next) => User.token
+//     .then(user => user.update({ gameroomId: request.body.gameroomId}))
+//     .then(user => response.send(user))
+//     .catch(next)
+// )
+
+// router.post("/events", auth, async (request, response) => {
+//   console.log(request.user.dataValues.id);
+//   const newEvent = { ...request.body, userId: request.user.dataValues.id };
+//   const event = await Event.create(newEvent);
+//   return response.status(201).send(event);
 // });
 
-router.put(
-  '/user',
-  (request, response, next) => User
-    .find(request.params.email)
-    .then(user => user.update({ gameroomId: request.body.gameroomId}))
-    .then(user => response.send(user))
-    .catch(next)
-)
-
+router.put('/join', auth, async(request, response, next) => {
+  console.log(request.user.dataValues.id);
+  const updatedUser = { ...request.body, gameroomId: request.user.dataValues.gameroomId};
+  const user = await User.update(updatedUser);
+  return response.send(user)
+})
 
 module.exports = router;
